a7ee0d11cb4631bbeebc40ac70c745a211d6957e
theeasylearn <theeasylearn@gmail.com>
2024-03-08 07:33:29 +0530

    forgot password

--- app2/src/AdminForgotPassword.js
+++ app2/src/AdminForgotPassword.js
@@ -1,6 +1,49 @@
+import {useState } from "react";
+import { Link } from "react-router-dom";
+import getBase from "./api";
+import axios from "axios";
+import showError, { showMessage } from "./toast-message";
+import { ToastContainer } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
 export default function AdminForgotPassword()
 {
+    //create state variable
+    let [email,setEmail] = useState();
+    let navigate = useNavigate();
+    let recoverAccount = function(e)
+    {
+        e.preventDefault();
+        console.log(email);
+        let apiAddress = getBase() + "/forgot_password.php?email=" + email;
+        axios.get(apiAddress,{
+            responseType:'json'
+        }).then((response) => {
+            console.log(response.data);
+            let error = response.data[0]['error'];
+            if(error !== 'no')
+              showError(error); 
+            else 
+            {
+                let success =  response.data[1]['success'];
+                let message =  response.data[2]['message'];
+                if(success === 'no')
+                  showError(message);
+                else 
+                {
+                  showMessage(message);
+                  setTimeout(() =>  {
+                      navigate("/");
+                  },2000);
+                }
+            }
+        }).catch((error) => 
+        {
+            showError('oops something went wrong, contact administrator');
+        });
+    }
     return (<div className="page-wrapper" id="main-wrapper" data-layout="vertical" data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed">
+      <ToastContainer />
     <div className="position-relative overflow-hidden bg-warning-subtle min-vh-100 d-flex align-items-center justify-content-center">
       <div className="d-flex align-items-center justify-content-center w-100">
         <div className="row justify-content-center w-100">
@@ -10,12 +53,13 @@ export default function AdminForgotPassword()
                 <h1 className="text-warning text-center fw-bold">Online shop</h1>
                 <hr />
                 <p className="text-center h5 fw-bold text-uppercase">Recover Account</p>
-                <form>
+                <form onSubmit={recoverAccount} >
                   <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={email} 
+                    onChange={(e) => setEmail(e.target.value)} />
                   </div>
-                  <a href="./index.html" className="btn btn-warning w-100 py-8 fs-4 mb-4 rounded-2">Recover account</a>
+                  <input type="submit" className="btn btn-warning w-100 py-8 fs-4 mb-4 rounded-2" value='Recover Account' />
                 </form>
               </div>
             </div>
